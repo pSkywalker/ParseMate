1d1afa0a7d1ccc20e25ebc03c85859a9d97dfef7
pSkywalker <34724562+pSkywalker@users.noreply.github.com>
2025-05-01 10:16:56 -0400

    Delete interfaces directory

--- interfaces/ArgSpec.ts
+++
@@ -1,7 +0,0 @@
-export type ArgSpec = {
-    flags: string[];          // e.g., ['-f', '--folder']
-    description: string;
-    required?: boolean;
-    multiple?: boolean;
-    default?: any;
-  };
